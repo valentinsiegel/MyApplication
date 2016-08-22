bcd4a8150a7530f1209154ac390d4f98bd3c9cde
Valenitn S <valentinsgl@gmail.com>
2016-08-22 21:00:08 +0200

    won't make the app crash

--- app/src/main/java/com/siegel/valenitn/myapplication/MainActivity.java
+++ app/src/main/java/com/siegel/valenitn/myapplication/MainActivity.java
@@ -2,12 +2,14 @@ package com.siegel.valenitn.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ((TextView) findViewById(R.id.test)).setText("de");
         setContentView(R.layout.activity_main);
     }
 }
