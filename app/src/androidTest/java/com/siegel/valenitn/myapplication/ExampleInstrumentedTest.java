364ca9d121af7c62e345b762c3afba5c1e75c1f2
Valenitn S <valentinsgl@gmail.com>
2016-08-22 21:11:20 +0200

    won't make the app crash

--- app/src/androidTest/java/com/siegel/valenitn/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/siegel/valenitn/myapplication/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.siegel.valenitn.myapplication", appContext.getPackageName());
+        assertEquals("com.siegel.valentin.myapplication", appContext.getPackageName());
     }
 }
